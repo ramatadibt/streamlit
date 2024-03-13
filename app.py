99dda99e62182117e4cb96ee4106f3b288b6d2da
ramatadibt <ramareddy.tadi@bugendaitech.com>
2024-03-13 18:49:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-col1,col2, col3, col4  = st.columns([0.2,0.32, 0.38, 0.1])
+col1,col2, col3, col4  = st.columns([0.2,0.35, 0.35, 0.1])
 with col1:
     st.image(Image.open('BugendaiTech Logo.png'))
 with col2:
