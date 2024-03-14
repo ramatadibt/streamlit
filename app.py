7962157aca6435233268344170145fb6fcb399aa
ramatadibt <ramareddy.tadi@bugendaitech.com>
2024-03-14 18:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,36 @@ import os
 HUGGINGFACEHUB_API_TOKEN =  st.secrets['HUGGINGFACEHUB_API_TOKEN']
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = HUGGINGFACEHUB_API_TOKEN
 
-
+from streamlit_card import card
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+if "card_states" not in st.session_state:
+    st.session_state.card_states = {
+        "prompt1": False,
+        "prompt2": False,
+        "prompt3": False,
+        "prompt4": False,
+        "prompt5": False,
+        "prompt6": False,
+    }
 
 
 st.set_page_config(page_title="Huggingface LLMs Chatbot", layout="wide")
 
 def reset_conversation():
   st.session_state.messages = []
+  st.session_state.card_states = {
+        "prompt1": False,
+        "prompt2": False,
+        "prompt3": False,
+        "prompt4": False,
+        "prompt5": False,
+        "prompt6": False,
+    }
+
 
 st.markdown("""
         <style>
@@ -72,10 +91,150 @@ llm_model = col3.selectbox('**Select LLM**', ["google/gemma-2b-it", "google/gemm
 col4.button('Clear Chat', on_click= reset_conversation)
 
 
+def handle_click(card_name):
+    st.session_state.card_states[card_name] = not st.session_state.card_states[card_name]
+
+col5, col6, col7,col8,col9,col10  = st.columns(6)
+
+with col5:
+    prompt1 = card(
+        title="Coding",
+        text="Write a python code to print fibonacci series",
+        image="https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg?auto=compress&cs=tinysrgb&w=600", 
+        on_click=lambda: handle_click("prompt1"),
+        styles={
+        "card": {
+            "width": "160px",
+            "height": "120px",
+            "border-radius": "2px",
+            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+            
+        },
+        "text": {
+            "font-family": "serif",
+        }
+    }
+        )
+    
+with col6:
+    prompt2 = card(
+        title="Cooking",
+        text="Give the step-by-step recipe to make a delicious pizza.",
+        image="https://images.pexels.com/photos/905847/pexels-photo-905847.jpeg?auto=compress&cs=tinysrgb&w=600", 
+        on_click=lambda: handle_click("prompt2"),
+        styles={
+        "card": {
+            "width": "160px",
+            "height": "120px",
+            "border-radius": "2px",
+            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+            
+        },
+        "text": {
+            "font-family": "serif",
+        }
+    }
+        )
+
+with col7:
+    prompt3 = card(
+        title="Mails",
+        text="Draft an email to a seller asking about computer specifications.",
+        image="https://images.unsplash.com/photo-1637593992672-ed85a851fdc3?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
+        on_click=lambda: handle_click("prompt3"),
+        styles={
+        "card": {
+            "width": "160px",
+            "height": "120px",
+            "border-radius": "2px",
+            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+            
+        },
+        "text": {
+            "font-family": "serif",
+            "font-size" : "14px",
+        }
+    }
+        )
+    
+
+with col8:
+    prompt4 = card(
+        title="Explain",
+        text="Teach me how a neural network works like I am 10 years old.",
+        image="https://images.pexels.com/photos/5212338/pexels-photo-5212338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", 
+        on_click=lambda: handle_click("prompt4"),
+        styles={
+        "card": {
+            "width": "160px",
+            "height": "120px",
+            "border-radius": "2px",
+            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+            
+        },
+        "text": {
+            "font-family": "serif",
+            "font-size" : "14px",
+        }
+    }
+        )
+    
+
+with col9:
+    prompt5 = card(
+        title="Brainstorming",
+        text="What are some off-the-beaten-path destinations to explore in India?",
+        image="https://miro.medium.com/v2/resize:fit:1000/1*QJ53LegShIfxuqljSB0w8Q.jpeg", 
+        on_click=lambda: handle_click("prompt5"),
+        styles={
+        "card": {
+            "width": "160px",
+            "height": "120px",
+            "border-radius": "2px",
+            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+            
+        },
+        "title":{
+            "font-size" : "22px", 
+    
+        },
+        "text": {
+            "font-family": "serif",
+            "font-size" : "14px",
+        }
+    }
+        )
+    
+with col10:
+    prompt6 = card(
+        title="Mentorship",
+        text="Act as Steve Jobs. Give me your advice in how can I improve my time management.",
+        image="https://ideas.ted.com/wp-content/uploads/sites/3/2018/09/featured_art_mentor_istock.jpg", 
+        on_click=lambda: handle_click("prompt6"),
+        styles={
+        "card": {
+            "width": "160px",
+            "height": "120px",
+            "border-radius": "2px",
+            "box-shadow": "0 0 10px rgba(0,0,0,0.5)",
+            
+        },
+        "title":{
+            "font-size" : "22px", 
+    
+        },
+        "text": {
+            "font-family": "serif",
+            "font-size" : "14px",
+        }
+    }
+        )
+
+
+st.write(prompt1, prompt2, prompt3, prompt4, prompt5, prompt6)
 
 llm = HuggingFaceEndpoint(
     repo_id=llm_model, 
-    # model_kwargs={"temperature": temperature, "max_new_tokens": max_tokens, "top_k": top_k, "load_in_8bit": True}
     temperature = 0.1,
     max_new_tokens = 1024,
     top_k = 50,
@@ -88,6 +247,84 @@ llm = HuggingFaceEndpoint(
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
+if prompt1:
+    p1 = "Write a python code to print fibonacci series"
+    st.chat_message("user").markdown(p1)
+    # Add user message to chat history
+    # st.session_state.messages.append({"role": "user", "content": p1})
+
+    response1 = llm(p1)
+    with st.chat_message("assistant"):
+        st.markdown(response1)
+    # st.chat_message("assistant").markdown(response1)
+    # Add assistant response to chat history
+    # st.session_state.messages.append({"role": "assistant", "content": response1})
+
+if prompt2:
+    p2 = "Give the step-by-step recipe to make a delicious pizza."
+    st.chat_message("user").markdown(p2)
+    # Add user message to chat history
+    # st.session_state.messages.append({"role": "user", "content": p2})
+    response2 = llm(p2)
+    with st.chat_message("assistant"):
+        st.markdown(response2)
+    # st.chat_message("assistant").markdown(response2)
+    # Add assistant response to chat history
+    # st.session_state.messages.append({"role": "assistant", "content": response2})
+
+if prompt3:
+    p3 = "Draft an email to a seller asking about computer specifications."
+    st.chat_message("user").markdown(p3)
+    # Add user message to chat history
+    # st.session_state.messages.append({"role": "user", "content": p3})
+    response3 = llm(p3)
+    with st.chat_message("assistant"):
+        st.markdown(response3)
+    # st.chat_message("assistant").markdown(response3)
+    # Add assistant response to chat history
+    # st.session_state.messages.append({"role": "assistant", "content": response3})
+
+
+if prompt4:
+    p4 = "Teach me how a neural network works like I am 10 years old."
+    st.chat_message("user").markdown(p4)
+    # Add user message to chat history
+    # st.session_state.messages.append({"role": "user", "content": p4})
+    response4 = llm(p4)
+    with st.chat_message("assistant"):
+        st.markdown(response4)
+    # st.chat_message("assistant").markdown(response4)
+    # Add assistant response to chat history
+    # st.session_state.messages.append({"role": "assistant", "content": response4})
+
+
+if prompt5:
+    p5 = "What are some off-the-beaten-path destinations to explore in India?"
+    st.chat_message("user").markdown(p5)
+    # Add user message to chat history
+    # st.session_state.messages.append({"role": "user", "content": p5})
+    response5 = llm(p5)
+    with st.chat_message("assistant"):
+        st.markdown(response5)
+    # st.chat_message("assistant").markdown(response5)
+    # Add assistant response to chat history
+    # st.session_state.messages.append({"role": "assistant", "content": response5})
+
+
+
+
+if prompt6:
+    p6 = "Act as Steve Jobs. Give me your advice in how can I improve my time management."
+    st.chat_message("user").markdown(p6)
+    # Add user message to chat history
+    # st.session_state.messages.append({"role": "user", "content": p6})
+    response6 = llm(p6)
+    with st.chat_message("assistant"):
+        st.markdown(response6)
+    # st.chat_message("assistant").markdown(response6)
+    # Add assistant response to chat history
+    # st.session_state.messages.append({"role": "assistant", "content": response6})
 
 
 # React to user input
