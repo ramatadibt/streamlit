75719351deab8fbac5fb7ecab430db7421b197b0
ramatadibt <ramareddy.tadi@bugendaitech.com>
2024-07-24 10:18:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def reset_conversation():
 st.markdown("""
         <style>
                .block-container {
-                    padding-top: 1rem;
+                    padding-top: 1.3rem;
                     padding-left: 3rem;
                     padding-right: 3rem;
                     padding-bottom: -1rem;
